d5804a01bc9c80ee05a0f90e3806338a27279664
uvardan <uvardan@utexas.edu>
2020-04-19 02:16:05 -0500

    Code Cleaning

--- src/main/java/ToDoAppSeleniumTest.java
+++ src/main/java/ToDoAppSeleniumTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class SeleniumConfig {
+public class ToDoAppSeleniumTest {
 
     public static void main(String[] args) throws InterruptedException {
 

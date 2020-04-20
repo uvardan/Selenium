952aaf2e701763acdd3bcdbdc752328008c8edb6
uvardan <uvardan@utexas.edu>
2020-04-19 20:36:53 -0500

    Code Cleaning

--- src/main/java/ToDoAppSeleniumTest.java
+++ src/main/java/ToDoAppSeleniumTest.java
@@ -9,10 +9,92 @@ import java.util.Queue;
 
 public class ToDoAppSeleniumTest {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
+        run();
+    }
 
-        WebDriver driver = getWebDriver();
+    public static void run() {
+        WebDriver driver = getHeadLessWebDriver();
+        login(driver);
+        todoListPage(driver);
+        addItems(driver);
+        deletingItems(driver);
+        updatingItems(driver);
+        navigateToHome(driver);
+        getWelcomeMessage(driver);
+        logout(driver);
+    }
 
+    private static void logout(WebDriver driver) {
+        System.out.println("Logging out .....");
+        driver.findElement(By.linkText("Logout")).click();
+        System.out.println("Logged out!");
+    }
+
+    private static void getWelcomeMessage(WebDriver driver) {
+        System.out.println("Welcome message clicked...");
+        driver.findElement(By.id("welcome")).click();
+        System.out.println("Welcome message displayed");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void navigateToHome(WebDriver driver) {
+
+        try {
+            System.out.println("Navigating to home page..");
+            Thread.sleep(3000);
+            driver.findElement(By.linkText("Home")).click();
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void updatingItems(WebDriver driver) {
+        try {
+            System.out.println("Updating item...");
+            Thread.sleep(3000);
+            driver.findElement(By.id("update")).click();
+            WebElement description = driver.findElement(By.id("updateDesc"));
+            description.clear();
+            Thread.sleep(3000);
+            description.sendKeys(" Updated");
+            WebElement date = driver.findElement(By.id("updateTarget"));
+            date.sendKeys("07072020");
+            driver.findElement(By.id("save")).click();
+            System.out.println("Item Updated!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void deletingItems(WebDriver driver) {
+        try {
+            Thread.sleep(3000);
+            System.out.println("Deleting item...");
+            driver.findElement(By.id("delete")).click();
+            System.out.println("Item Deleted!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void todoListPage(WebDriver driver) {
+        try {
+            System.out.println("Navigating to todo page");
+            driver.findElement(By.id("todo")).click();
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void login(WebDriver driver) {
         // Load and Login TodoApp
         System.out.println("Welcome to Todo App");
         WebElement password = driver.findElement(By.name("password"));
@@ -20,60 +102,22 @@ public class ToDoAppSeleniumTest {
         password.sendKeys("dummy");
         System.out.println("Attempting to Login...");
         login.click();
-        System.out.println("Logged in");
-        Thread.sleep(3000);
-
-        // TodoList Page
-        System.out.println("Navigating to todo page");
-        driver.findElement(By.id("todo")).click();
-        Thread.sleep(3000);
-
-
-        // Adding items to TodoList
-        System.out.println("Adding Items to TodoList");
-        addItems(driver);
-        System.out.println("Items added!");
-
-
-        // Deleting Items
-        Thread.sleep(3000);
-        System.out.println("Deleting item...");
-        driver.findElement(By.id("delete")).click();
-        System.out.println("Item Deleted!");
-
-        // Updating Items
-        System.out.println("Updating item...");
-        Thread.sleep(3000);
-        driver.findElement(By.id("update")).click();
-        WebElement description = driver.findElement(By.id("updateDesc"));
-        description.clear();
-        Thread.sleep(3000);
-        description.sendKeys(" Updated");
-        WebElement date = driver.findElement(By.id("updateTarget"));
-        date.sendKeys("07072020");
-        driver.findElement(By.id("save")).click();
-        System.out.println("Item Updated!");
-
-        // Navigate to Home
-        System.out.println("Navigating to home page..");
-        Thread.sleep(3000);
-        driver.findElement(By.linkText("Home")).click();
-        Thread.sleep(3000);
-
-        // Welcome Message
-        System.out.println("Welcome message clicked...");
-        driver.findElement(By.id("welcome")).click();
-        System.out.println("Welcome message displayed");
-        Thread.sleep(3000);
-
-        // Logout
-        System.out.println("Logging out .....");
-        driver.findElement(By.linkText("Logout")).click();
-        System.out.println("Logged out!");
-
+        System.out.println("Login successful!");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     private static WebDriver getWebDriver() {
+        System.setProperty("webdriver.chrome.driver", "/Users/utkarsh/driver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://localhost:4200/");
+        return driver;
+    }
+
+    private static WebDriver getHeadLessWebDriver() {
         System.setProperty("webdriver.chrome.driver", "/Users/utkarsh/driver/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200", "--ignore-certificate-errors");
@@ -83,6 +127,7 @@ public class ToDoAppSeleniumTest {
     }
 
     private static void addItems(WebDriver driver) {
+        System.out.println("Adding Items to TodoList");
         Queue<String> items = getItems();
         int i = 0;
 
@@ -102,6 +147,7 @@ public class ToDoAppSeleniumTest {
             System.out.println("Item " + i + " added. ");
 
         }
+        System.out.println("All items added to TodoList");
     }
 
     private static Queue<String> getItems() {
